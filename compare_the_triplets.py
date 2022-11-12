6f6a1924e09fde7309824a5059ec40a8f684a59e
nathalia-santos <nathaliasantospbg@hotmail.com>
2022-11-12 09:34:40 -0300

    compareTriplets

--- compare_the_triplets.py
+++ compare_the_triplets.py
@@ -1,15 +1,16 @@
 def compareTriplets(a, b):
-    points_of_winner = []
     sum_points_a = 0
     sum_points_b = 0
     for i, j in zip(a, b):
-        if a[i] > b[j]:
+        if i > j:
             sum_points_a += 1
+        elif i == j:
+            pass
         else:
             sum_points_b += 1
     return sum_points_a, sum_points_b
 
-print(compareTriplets((17, 28, 30,), (99, 16, 8)))
+print(compareTriplets([17, 28, 30,],[99, 16, 8]))
 
 
 
